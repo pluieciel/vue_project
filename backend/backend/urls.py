aecd7b5d5ab513270d2f69a55daadc7f653419a6
pluieciel <yuzhao@c1r3s2.42luxembourg.lu>
2024-12-06 14:11:47 +0100

    ignore

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,6 +19,4 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/chat/', include('Chat.routing')),  # Chat app WebSocket routes
-    path('api/game/', include('Game.routing')),  # Game app WebSocket routes
 ]
--- docker/django/tools/start_django.sh
+++ docker/django/tools/start_django.sh
@@ -2,5 +2,6 @@
 
 python manage.py makemigrations
 python manage.py migrate
+python manage.py collectstatic --noinput
 
 exec daphne -b 0.0.0.0 -p 8000 backend.asgi:application
